b0b314722c4c8293caf74e7b61fc2c8f740b7234
claudio → vecna <claudio@tracking.exposed>
2017-03-20 18:03:24 -0300

    fixed bugs and implemented as app

---
+++ routes/getCampaignIndex.js
@@ -0,0 +1,34 @@
+var _ = require('lodash');
+var debug = require('debug')('route:getCampaignIndex');
+var pug = require('pug');
+var nconf = require('nconf').env();
+
+var campaignName = nconf.get('campaign');
+
+var pugCompiler = function(filePrefix) {
+    return pug.compileFile(
+        __dirname + '/../campaigns/' + campaignName + '/' + filePrefix + '.pug', {
+            pretty: true,
+            debug: false
+        }
+    );
+};
+
+var pageMap = {
+    'home': pugCompiler('pugs/pagestruct'),
+    'landing': pugCompiler('pugs/pagestruct'),
+    'what-to-do': pugCompiler('pugs/pagestruct'),
+    'about': pugCompiler('pugs/pagestruct'),
+    'archive': pugCompiler('pugs/pagestruct'),
+    'site': pugCompiler('pugs/pagestruct')
+};
+
+var getCampaignIndex = function(req) {
+
+    var pageName = _.get(req.params, 'page');
+    debug("page name %s", pageName);
+
+    return { 'text': pageMap[pageName]() };
+};
+
+module.exports = getCampaignIndex;
