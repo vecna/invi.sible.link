9ebbf14bc079db991067ce68406e9acc4829f60e
Claudio invi.sible.link <claudio@tracking.exposed>
2017-06-30 14:52:04 +0000

    simply coordination scripts for daily test

---
+++ campaign-wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "irantrex"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "itatopex"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "clinics-BR"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "clinics-CO"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "gptrex"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "irantrex"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "culture"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "travel"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "halal"
+DEBUG=* bin/campaignChecker.js --config config/campaignChecker.json --campaign "mosques"
+
---
+++ director-wrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+csv=campaigns/irantrex/iran1st.csv taskName=irantrex DEBUG=* bin/directionTool.js  
+csv=campaigns/chuptrex/clinics-BR.csv taskName=clinics-BR DEBUG=* bin/directionTool.js  
+csv=campaigns/chuptrex/clinics-CO.csv taskName=clinics-CO DEBUG=* bin/directionTool.js  
+DEBUG=* bin/directionTool.js --csv campaigns/amtrex/halal-list.csv --taskName halal 
+DEBUG=* bin/directionTool.js --csv campaigns/amtrex/culture-list.csv --taskName culture 
+DEBUG=* bin/directionTool.js --csv campaigns/amtrex/mosques-list.csv --taskName mosques 
+DEBUG=* bin/directionTool.js --csv campaigns/amtrex/travel-list.csv --taskName travel 
+csv=campaigns/itatopex/lista.csv taskName=itatopex DEBUG=* bin/directionTool.js 
+csv=campaigns/gptrex/gptrex.csv taskName=gptrex DEBUG=* bin/directionTool.js 
