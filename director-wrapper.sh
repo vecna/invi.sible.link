82245e318ce596cd0cda7edac0acdb6acf8966c1
Claudio invi.sible.link <claudio@tracking.exposed>
2017-07-06 19:59:26 +0000

    extended campaign CL and MX

--- director-wrapper.sh
+++ director-wrapper.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 csv=campaigns/irantrex/iran1st.csv taskName=irantrex DEBUG=* bin/directionTool.js  
+csv=campaigns/chuptrex/clinics-MX.csv taskName=clinics-MX DEBUG=* bin/directionTool.js  
+csv=campaigns/chuptrex/clinics-CL.csv taskName=clinics-CL DEBUG=* bin/directionTool.js  
 csv=campaigns/chuptrex/clinics-BR.csv taskName=clinics-BR DEBUG=* bin/directionTool.js  
 csv=campaigns/chuptrex/clinics-CO.csv taskName=clinics-CO DEBUG=* bin/directionTool.js  
 DEBUG=* bin/directionTool.js --csv campaigns/amtrex/halal-list.csv --taskName halal 
