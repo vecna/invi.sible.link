50d7040acae499bf010a5d7a2bdea8d253327b7c
Claudio invi.sible.link <claudio@tracking.exposed>
2017-12-03 22:14:49 +0000

    this is not how code is maintain

--- analyzer-unrolled.sh
+++ analyzer-unrolled.sh
@@ -1,39 +1,52 @@
 #!/bin/sh
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "irantrex"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "irantrex"
+DEBUG=* bin/analyzeGroup.js --campaign irantrex --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "itatopex"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "itatopex"
+DEBUG=* bin/analyzeGroup.js --campaign itatopex --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "clinics-BR"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "clinics-BR"
+DEBUG=* bin/analyzeGroup.js --campaign clinics-BR --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "clinics-CO"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "clinics-CO"
+DEBUG=* bin/analyzeGroup.js --campaign clinics-CO --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "clinics-MX"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "clinics-MX"
+DEBUG=* bin/analyzeGroup.js --campaign clinics-MX --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "clinics-CL"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "clinics-CL"
+DEBUG=* bin/analyzeGroup.js --campaign clinics-CL --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "gptrex"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "gptrex"
+DEBUG=* bin/analyzeGroup.js --campaign gptrex --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "culture"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "culture"
+DEBUG=* bin/analyzeGroup.js --campaign culture --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "travel"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "travel"
+DEBUG=* bin/analyzeGroup.js --campaign travel --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "halal"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "halal"
+DEBUG=* bin/analyzeGroup.js --campaign halal --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "mosques"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "mosques"
+DEBUG=* bin/analyzeGroup.js --campaign mosques --daysago 0
 
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "poland"
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "poland"
+DEBUG=* bin/analyzeGroup.js --campaign poland --daysago 0
 
 DEBUG=* bin/analyzeBadger.js --config config/analyzerProduction.json --campaign "ngos"
 DEBUG=* bin/analyzePhantom.js --config config/analyzerProduction.json --campaign "ngos"
+DEBUG=* bin/analyzeGroup.js --campaign ngos --daysago 0
