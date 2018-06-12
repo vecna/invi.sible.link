2443b42ec9e1b2fa39531df6dac9ad92b5cdfa07
Claudio Agosti <claudio@tracking.exposed>
2018-06-12 18:20:16 +0200

    small js to send updates

---
+++ sections/webscripts/kzbrg.js
@@ -0,0 +1,10 @@
+
+var init = function() {
+
+    $('#suggest').on('click', function(e) {
+        var site = $("#newsite").val();
+        var desc = $("#whereis").val();
+        console.log(site, desc);
+    });
+
+};
