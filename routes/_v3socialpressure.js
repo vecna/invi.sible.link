23de0aef3894f39c702faf23eae4f9eae75ebf23
Claudio Agosti <claudio@tracking.exposed>
2018-06-18 12:16:52 +0200

    routes/_v3socialpressure.js

---
+++ routes/_v3socialpressure.js
@@ -0,0 +1,5 @@
+module.exports = {
+    /* this do not include anything additional for the v3, just a subset of storyteller */
+    getGooglesOnly:        require('./getGooglesOnly'),
+    getSurface:            require('./getSurface'),
+};
