13c9801de8f41a78acc216411a8c2e0ceb1744f6
Claudio Agosti <claudio@tracking.exposed>
2018-09-13 16:39:17 +0200

    small fixes

--- bin/monoSite.sh
+++ bin/monoSite.sh
@@ -6,7 +6,7 @@ if [ -z $site ]; then
     echo "required as argument a website"
     exit 1
 fi
-echo -e "\n`date` -> $site" >> /tmp/monoSiteAdd.log
+echo "`date` -> $site" >> /tmp/monoSiteAdd.log
 
 DEBUG=* bin/queueSite.js --url $site --kind basic
 amount=1 concurrency=1 timeout=20 site=$site npm run phantom
