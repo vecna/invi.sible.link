8fc1ed6d7217c33fb96ff347d484e321c5bd5503
Claudio Agosti <claudio@tracking.exposed>
2018-11-08 16:48:28 +0100

    reduced the getSites fields

--- routes/getSites.js
+++ routes/getSites.js
@@ -9,11 +9,15 @@ var mongo = require('../lib/mongo');
 function getSites(req) {
 
     var filter = { campaign: req.params.campaign };
+    var stripFields = ['_id', 'id', 'lastResultId', 'lastSurfId', 'lastChecked', 'frequency' ];
 
     debug("getSites filter %j", filter);
 
     return mongo
         .read(nconf.get('schema').sites, filter)
+        .map(function(s) {
+            return _.omit(s, stripFields);
+        })
         .then(function(sites) {
             debug("getSites returns: %d", _.size(sites));
             return {
