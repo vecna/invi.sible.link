dd236a2d3bf14aea849ec7598faa2deb86538a67
Claudio <claudio@tracking.exposed>
2018-11-09 17:12:26 +0100

    allow origin *

--- bin/v3-social-pressure.js
+++ bin/v3-social-pressure.js
@@ -49,6 +49,14 @@ console.log( "http://" + nconf.get('interface') + ':' + nconf.get('port') + " li
 /* ------------------------------------------------------------ 
                     IMPLEMENTED ROUTES
    ------------------------------------------------------------ */
+
+// WARNING -- remind 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/api/v:version/results/:campaign', function(req, res) {
     return dispatchPromise('getResults', routes, req, res);
 });
